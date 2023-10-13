8cb7c12ad39c2a26c7614b398add6bba2233731c
Pavel <pavel@b2profit.ru>
2023-10-13 22:42:13 +0300

    Решил первую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");
+}
+if(firstNumber < secondNumber)
+{
+    Console.WriteLine($"Первое число {firstNumber} меньше чем второе число {secondNumber}");
+}
+if(firstNumber == secondNumber)
+{
+    Console.WriteLine($"Первое и второе число равны {firstNumber}");
+}
