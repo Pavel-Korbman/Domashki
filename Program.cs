831e81ffe99dc2de9fc5a38f5872b6039c3c2056
Pavel <pavel@b2profit.ru>
2023-10-14 01:27:18 +0300

    Добавил Апострофы при выводе

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ Console.WriteLine("Введите второе число:");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 if(firstNumber > secondNumber)
 {
-    Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");
+    Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
 }
 if(firstNumber < secondNumber)
 {
-    Console.WriteLine($"Первое число {firstNumber}' меньше чем второе число {secondNumber}");
+    Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
 }
 if(firstNumber == secondNumber)
 {
-    Console.WriteLine($"Первое и второе число равны {firstNumber}");
+    Console.WriteLine($"Первое и второе число равны `{firstNumber}`");
 }
